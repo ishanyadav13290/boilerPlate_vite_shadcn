525cf01e39f9d5d2f4581ba6c175215614be8e45
Ishan Yadav <ishanyadav13290@gmail.com>
2025-07-15 10:51:09 +0530

    boilerplate setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Razor Pay Integration</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
